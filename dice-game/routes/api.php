34e28accd034916a21ad260a7487af7fba62856f
Oscar <oscanguera@gmail.com>
2024-09-18 11:21:35 +0200

    Creadas las rutas de basicas crud jugadores

--- dice-game/routes/api.php
+++ dice-game/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+//mostramos todos los jugadores
+Route::get('/players',[UserController::class, 'DisplayAllPlayers']);
+//crear jugador
+Route::post('/players',[UserController::class, 'CreatePlayer']);
+//mostrar un jugador
+Route::get('/players{id}',[UserController::class, 'DisplayPlayer']);
+//Editar jugador
+Route::put('/players{id}',[UserController::class, 'UpdatePlayer']);
+//Eliminar Jugador
+Route::delete('/players{id}',[UserController::class, 'DeletePlayer']);
