760d1b316081a2949cc6364f071df6ff7fec68c7
Oscar <oscanguera@gmail.com>
2024-09-18 11:23:35 +0200

    Creadas las rutas de basicas crud jugadores

--- dice-game/routes/api.php
+++ dice-game/routes/api.php
@@ -7,14 +7,16 @@ use Illuminate\Support\Facades\Route;
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
+Route::group(['middleware' => 'auth:api'], function (){
+    //mostramos todos los jugadores
+    Route::get('/players',[UserController::class, 'DisplayAllPlayers']);
+    //crear jugador
+    Route::post('/players',[UserController::class, 'CreatePlayer']);
+    //mostrar un jugador
+    Route::get('/players{id}',[UserController::class, 'DisplayPlayer']);
+    //Editar jugado
+    Route::put('/players{id}',[UserController::class, 'UpdatePlayer']);
+    //Eliminar Jugador
+    Route::delete('/players{id}',[UserController::class, 'DeletePlayer']);
+});
 
-//mostramos todos los jugadores
-Route::get('/players',[UserController::class, 'DisplayAllPlayers']);
-//crear jugador
-Route::post('/players',[UserController::class, 'CreatePlayer']);
-//mostrar un jugador
-Route::get('/players{id}',[UserController::class, 'DisplayPlayer']);
-//Editar jugador
-Route::put('/players{id}',[UserController::class, 'UpdatePlayer']);
-//Eliminar Jugador
-Route::delete('/players{id}',[UserController::class, 'DeletePlayer']);
