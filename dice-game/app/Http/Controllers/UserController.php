28a14d8b65d80d12707e68ff3d694412d4e1e6a0
Oscar <oscanguera@gmail.com>
2024-09-19 11:33:06 +0200

    he creado crud para usuarios

--- dice-game/app/Http/Controllers/UserController.php
+++ dice-game/app/Http/Controllers/UserController.php
@@ -1,10 +1,59 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate(
+            [
+                'nickname' => ['nullable', 'string', 'max:100'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string' ]
+            ]
+        );
+        $user = User::create(
+            [
+                'nickname' => $request->nickname,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]
+        );
+        $token=$user->createToken('auth_token')->accessToken;
+
+        return response([
+            'token'=> $token
+        ]);
+    }
+    public function login(Request $request){
+        $request->validate(
+            [
+                'email' => ['required'],
+                'password' => ['required']
+            ]
+        );
+        $user=User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message'=>'Los datos introducidos no son correctos'
+            ]);
+        }
+        $token=$user->createToken('auth_token')->accessToken;
+
+        return response([
+            'token'=> $token
+        ]);
+    }
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+
+        return response([
+            'message'=>'La salido de la sesión con exito'
+        ]);
+    }
+
 }
